1f8b9bd1512639a94d1e71572a47b2bfbed2c451
Luis Olivares <luis.olivares.info@gmail.com>
2020-05-07 14:59:17 -0400

    Se realiza formateo visual de videos con sus comentarios

--- videos-laravel/resources/views/video/comments.blade.php
+++ videos-laravel/resources/views/video/comments.blade.php
@@ -1,23 +1,37 @@
-<h4>Comentarios</h4>
-@if(session('message'))
-  <div class="alert alert-success">
-      {{session('message')}}
-  </div>
-@endif
-@if(Auth::check())
-<form class="col-md-4" action="{{url('/comment')}}" method="post">
-  {!! csrf_field() !!}
-  <input type="hidden" name="video_id" value="{{$video->id}}" required>
-  <p>
-      <textarea name="body" class="form-control" required></textarea>
-  </p>
-  <input type="submit" class="btn btn-success" value="Comentar">
-</form>
-@endif
+<div class="content">
 
-@if(isset($video->comments))
-  @foreach($video->comments as $comment)
-    {{$comment->user->name.' '.$comment->user->surname}} hace {{\FormatTime::LongTimeFilter($comment->created_at)}}
-    {{$comment->body}}
-  @endforeach
-@endif
+  @if(session('message'))
+    <div class="alert alert-success">
+        {{session('message')}}
+    </div>
+  @endif
+  <div class="card" id="nuevo-comentario" style="max-width:80%;">
+    <div class="card-body">
+      <h4 class="card-title">Comentarios</h4>
+      @if(Auth::check())
+      <form action="{{url('/comment')}}" method="post">
+        {!! csrf_field() !!}
+        <input type="hidden" name="video_id" value="{{$video->id}}" required>
+        <textarea placeholder="Escribe tu comentario aquí" class="pb-cmnt-textarea" name="body" required></textarea>
+        <input type="submit" class="btn btn-success" value="Comentar">
+      </form>
+      @endif
+      <div class="row"><div class="col-md-12"><hr></div></div>
+      <div class="row">
+        @if(isset($video->comments))
+          @foreach($video->comments as $comment)
+          <div class="col-md-6">
+            {{$comment->user->name.' '.$comment->user->surname}} hace {{\FormatTime::LongTimeFilter($comment->created_at)}}
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            {{$comment->body}}
+          </div>
+          </div>
+          @endforeach
+        @endif
+
+    </div>
+  </div>
+</div>
--- videos-laravel/resources/views/video/detail.blade.php
+++ videos-laravel/resources/views/video/detail.blade.php
@@ -7,9 +7,10 @@
       <h5 class="card-title">{{$video->title}}</h5>
       <video controls src="{{route ('fileVideo',['filename' => $video->video_path])}}" autoplay width="100%">
       </video>
-      <p class="card-text">  Subido por el usuario {{$video->user->name.' '.$video->user->surname}} el día {{\FormatTime::LongTimeFilter($video->created_at)}}</p>
+      <p class="card-text">  Subido por el usuario {{$video->user->name.' '.$video->user->surname}}  {{\FormatTime::LongTimeFilter($video->created_at)}}</p>
     </div>
   </div>
+
 @include('video.comments')
 </div>
 @endsection
