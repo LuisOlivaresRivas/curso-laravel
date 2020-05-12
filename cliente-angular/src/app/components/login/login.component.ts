55467f5a00b34e07e5522d4763545fbbc80c9110
Luis Olivares <luis.olivares.info@gmail.com>
2020-05-12 17:51:26 -0400

    Login de usuario y persisitencia de la sesion

--- cliente-angular/src/app/components/login/login.component.ts
+++ cliente-angular/src/app/components/login/login.component.ts
@@ -1,23 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { User } from '../../models/user';
+import { UserService } from '../../services/user.service';
+
+
 
 @Component({
     // tslint:disable-next-line: component-selector
     selector: 'login',
-    templateUrl: './login.component.html'
+    templateUrl: './login.component.html',
+    providers: [UserService]
 })
 export class LoginComponent implements OnInit{
-    title: string;
-
+    public title: string;
+    public user: User;
+    public token;
+    public identity;
     constructor(
-        // tslint:disable-next-line: variable-name
-        private _route: ActivatedRoute,
-        // tslint:disable-next-line: variable-name
-        private _router: Router
+        private _userService: UserService,
+        // private _route: ActivatedRoute,
+        // private _router: Router
     ){
         this.title = 'Identificate';
+        this.user = new User(1, 'ROLE_USER', '', '', '', '');
     }
     ngOnInit(){
         console.log('login.component cargado correctamente!!');
+        let user = this._userService.getIdentity();
+        console.log(user.name);
+    }
+    onSubmit(form){
+        console.log(this.user);
+
+        this._userService.signup(this.user).subscribe(
+            response => {
+                // Token
+                this.token = response;
+                localStorage.setItem('token', this.token);
+                this._userService.signup(this.user, true).subscribe(
+                    response => {
+                        this.identity = response;
+                        localStorage.setItem('identity', JSON.stringify(this.identity));
+                    }, error => {
+                        console.log(<any>error);
+                    }
+                );
+            },
+            error => {
+                console.log(<any>error);
+            }
+        );
     }
 }
